9087de4ef913a261f3a1d0e04f6f4d4c6c59bfb9
George <71434803+george-swift@users.noreply.github.com>
2021-05-03 21:58:23 +0100

    Refactor addtolibrary function

--- script.js
+++ script.js
@@ -12,116 +12,125 @@ let newBook;
 let bookIds = 0;
 
 function Book(id, title, author, pages) {
-    this.id = id;
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.isRead = false;
+  this.id = id;
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.isRead = false;
 }
 
 function addBookToLibrary() {
-    newBook = new Book(
-        title.value,
-        author.value,
-        pages.value,
-        read.value,
-    );
+  newBook = new Book();
+  bookIds += 1;
+  newBook.id = bookIds;
+  newBook.title = title.value;
+  newBook.author = author.value;
+  newBook.pages = pages.value;
 }
 
 function toggle(e) {
-    if (e.target.textContent = 'Not Read') {
-        e.target.textContent = 'Read';
-        myLibrary[e.target.dataset.id].isRead = true;
-    }
+  if (e.target.textContent === 'Not Read') {
+    e.target.textContent = 'Read';
+    myLibrary[e.target.dataset.id].isRead = true;
+  } else {
+    myLibrary[e.target.dataset.id].isRead = false;
+  }
 }
 
+function tableRow() {
+  const tr = document.createElement('tr');
+  const tdTitle = document.createElement('td');
+  const tdAuthor = document.createElement('td');
+  const tdPages = document.createElement('td');
+  const tdBtn = document.createElement('td');
+  const tdBtnRead = document.createElement('td');
 
-function removeBook(e) {
-    myLibrary.splice(e.target.dataset.id, 1);
-    const table = document.querySelector('.table');
-    table.remove();
-    displayBooks()
+  const removeBtn = document.createElement('button');
+  const readBtn = document.createElement('button');
+
+  return {
+    tr,
+    tdTitle,
+    tdAuthor,
+    tdPages,
+    tdBtn,
+    tdBtnRead,
+    removeBtn,
+    readBtn,
+  };
 }
 
 function displayBooks() {
-    const container = document.querySelector('#container');
-    const table = document.createElement('table');
-    table.classList.add('table');
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
-    const thTitle = document.createElement('th');
-    const thAuthor = document.createElement('th');
-    const thPages = document.createElement('th');
+  const container = document.querySelector('#container');
+  const table = document.createElement('table');
+  table.classList.add('table');
+  const thead = document.createElement('thead');
+  const tr = document.createElement('tr');
+  const thTitle = document.createElement('th');
+  const thAuthor = document.createElement('th');
+  const thPages = document.createElement('th');
 
-    const thBtn = document.createElement('th');
-    const thBtnRead = document.createElement('th');
-    thTitle.textContent = 'Title';
-    thAuthor.textContent = 'Author';
-    thPages.textContent = 'Pages';
+  const thBtn = document.createElement('th');
+  const thBtnRead = document.createElement('th');
+  thTitle.textContent = 'Title';
+  thAuthor.textContent = 'Author';
+  thPages.textContent = 'Pages';
 
-    thBtn.textContent = '';
-    thBtnRead.textContent = '';
+  thBtn.textContent = '';
+  thBtnRead.textContent = '';
 
-    const tbody = document.createElement('tbody');
+  const tbody = document.createElement('tbody');
 
-    for (let i = 0; i < myLibrary.length; i += 1) {
-        const tr = document.createElement('tr');
-        const tdTitle = document.createElement('td');
-        const tdAuthor = document.createElement('td');
-        const tdPages = document.createElement('td');
-        const tdBtn = document.createElement('td');
-        const tdBtnRead = document.createElement('td');
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const table = tableRow();
 
-        const removeBtn = document.createElement('button');
-        const readBtn = document.createElement('button');
-
-        tdTitle.textContent = myLibrary[i].title;
-        tdAuthor.textContent = myLibrary[i].author;
-        tdPages.textContent = myLibrary[i].pages;
-        tdBtn.appendChild(removeBtn);
-        tdBtnRead.appendChild(readBtn);
-        removeBtn.classList.add(...btnClass);
-        readBtn.classList.add(...readBtnClass);
-        removeBtn.textContent = 'Remove';
-        removeBtn.setAttribute('data-id', `${i}`);
-        if (myLibrary[i].isRead)
-            readBtn.textContent = 'Read';
-        else
-            readBtn.textContent = 'Not Read';
-        readBtn.setAttribute('data-id', `${i}`);
-        readBtn.addEventListener('click', toggle);
-        removeBtn.addEventListener('click', removeBook);
-        tr.append(tdTitle, tdAuthor, tdPages, tdBtn, tdBtnRead);
-        tbody.appendChild(tr);
+    table.tdTitle.textContent = myLibrary[i].title;
+    table.tdAuthor.textContent = myLibrary[i].author;
+    table.tdPages.textContent = myLibrary[i].pages;
+    table.tdBtn.appendChild(table.removeBtn);
+    table.tdBtnRead.appendChild(table.readBtn);
+    table.removeBtn.classList.add(...btnClass);
+    table.readBtn.classList.add(...readBtnClass);
+    table.removeBtn.textContent = 'Remove';
+    table.removeBtn.setAttribute('data-id', `${i}`);
+    if (myLibrary[i].isRead) {
+      table.readBtn.textContent = 'Read';
+    } else {
+      table.readBtn.textContent = 'Not Read';
     }
+    table.readBtn.setAttribute('data-id', `${i}`);
+    table.readBtn.addEventListener('click', toggle);
+    table.removeBtn.addEventListener('click', (e) => {
+      myLibrary.splice(e.target.dataset.id, 1);
+      const table = document.querySelector('.table');
+      table.remove();
+      displayBooks();
+    });
+    table.tr.append(table.tdTitle, table.tdAuthor, table.tdPages, table.tdBtn, table.tdBtnRead);
+    tbody.appendChild(table.tr);
+  }
 
-    container.appendChild(table);
-    table.appendChild(thead);
-    thead.appendChild(tr);
-    tr.append(thTitle, thAuthor, thPages, thBtn, thBtnRead);
-    table.appendChild(tbody);
+  container.appendChild(table);
+  table.appendChild(thead);
+  thead.appendChild(tr);
+  tr.append(thTitle, thAuthor, thPages, thBtn, thBtnRead);
+  table.appendChild(tbody);
 }
 
 const saveBtn = document.querySelector('#btnSave');
 
-saveBtn.addEventListener('click', event => {
-    let newBook = new Book();
-    bookIds += 1;
-    newBook.id = bookIds;
-    newBook.author = document.querySelector('#author').value;
-    newBook.title = document.querySelector('#title').value;
-    newBook.pages = document.querySelector('#pages').value;
+saveBtn.addEventListener('click', () => {
+  addBookToLibrary();
+  myLibrary.push(newBook);
+  $('#exampleModal').modal('hide');
+  const table = document.querySelector('.table');
+  table.remove();
 
-    myLibrary.push(newBook);
-    $('#exampleModal').modal('hide');
-    const table = document.querySelector('.table');
-    table.remove();
+  $('#exampleModal').on('hidden.bs.modal', function() {
+    $(this).find('form').trigger('reset');
+  });
 
-    $('#exampleModal').on('hidden.bs.modal', function() {
-        $(this).find('form').trigger('reset');
-    });
-
-    displayBooks();
+  displayBooks();
 });
 
 displayBooks();
