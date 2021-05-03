e2b968fa4ccffe0526e1c5f882cd835d362f54d2
ybadmus <yusif.badmus@gmail.com>
2021-05-03 23:41:02 +0100

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # Library
 This is the very first project of the JavaScript module. It is preceded by the lesson about “Objects” and the “Object Constructor”.
+
+![screenshot](https://raw.githubusercontent.com/ybadmus/Library/feature/app_screenshot.PNG)
+![screenshot](https://raw.githubusercontent.com/ybadmus/Library/feature/app_screenshot_2.PNG)
+
+## Built With
+
+- HTML, Bootstrap, Javascript
+- Using Stylelint
+- Git, GitHub
+
+## Live Demo
+
+ [Live demo link](http://www.ybadmus.me/Library/)
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+- Clone the project unto your local machine
+- Open up the .html file.
+
+## Prerequisites
+Before you continue, be sure that you have installed:
+
+- A Git GUI.
+- An Integrated Development Environment.
+- Chrome, Firefox or Opera. Internet Explorer is not supported.
+
+## Authors
+
+👤 Yusif Badmus
+
+- GitHub: [@ybadmus](https://github.com/ybadmus) 
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/ybadmus/)
+- Twitter: [@yusif_badmus](https://twitter.com/yusif_badmus)
+
+👤 Ubong George
+
+- GitHub: [@george-swift](https://github.com/george-swift) 
+- Twitter: [@__pragmaticdev](https://twitter.com/__pragmaticdev)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/ybadmus/welfare_app/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [Microverse](https://www.microverse.org)
+- [Odin-Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/dom-manipulation)
+- [Javascript-MSDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes)
+- Our standup teams
+
+## 📝 License
+
+<p>This project is <a href="LICENSE">MIT</a> licensed.</p>
+
+## Enjoy!
--- script.js
+++ script.js
@@ -7,149 +7,149 @@ const btnClass = ['btn', 'btn-danger'];
 const readBtnClass = ['btn', 'btn-success'];
 const saveBtn = document.querySelector('#btnSave');
 
-const myLibrary = [];
+let myLibrary = [];
 let newBook;
 let bookIds = 0;
 
 function Book(id, title, author, pages) {
-  this.id = id;
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.isRead = false;
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = false;
 }
 
 function addBookToLibrary() {
-  newBook = new Book();
-  bookIds += 1;
-  newBook.id = bookIds;
-  newBook.title = title.value;
-  newBook.author = author.value;
-  newBook.pages = pages.value;
+    newBook = new Book();
+    bookIds += 1;
+    newBook.id = bookIds;
+    newBook.title = title.value;
+    newBook.author = author.value;
+    newBook.pages = pages.value;
 }
 
 function toggle(e) {
-  if (e.target.textContent === 'Not Read') {
-    e.target.textContent = 'Read';
-    myLibrary[e.target.dataset.id].isRead = true;
-  } else {
-    myLibrary[e.target.dataset.id].isRead = false;
-  }
+    if (e.target.textContent === 'Not Read') {
+        e.target.textContent = 'Read';
+        myLibrary[e.target.dataset.id].isRead = true;
+    } else {
+        myLibrary[e.target.dataset.id].isRead = false;
+    }
 }
 
 function tableHead() {
-  const container = document.querySelector('#container');
-  const table = document.createElement('table');
-  table.classList.add('table');
-  const thead = document.createElement('thead');
-  const tr = document.createElement('tr');
-  const thTitle = document.createElement('th');
-  const thAuthor = document.createElement('th');
-  const thPages = document.createElement('th');
-  const thBtn = document.createElement('th');
-  const thBtnRead = document.createElement('th');
+    const container = document.querySelector('#container');
+    const table = document.createElement('table');
+    table.classList.add('table');
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    const thTitle = document.createElement('th');
+    const thAuthor = document.createElement('th');
+    const thPages = document.createElement('th');
+    const thBtn = document.createElement('th');
+    const thBtnRead = document.createElement('th');
 
-  thTitle.textContent = 'Title';
-  thAuthor.textContent = 'Author';
-  thPages.textContent = 'Pages';
-  thBtn.textContent = '';
-  thBtnRead.textContent = '';
+    thTitle.textContent = 'Title';
+    thAuthor.textContent = 'Author';
+    thPages.textContent = 'Pages';
+    thBtn.textContent = '';
+    thBtnRead.textContent = '';
 
-  return {
-    container,
-    table,
-    thead,
-    tr,
-    thTitle,
-    thAuthor,
-    thPages,
-    thBtn,
-    thBtnRead,
-  };
+    return {
+        container,
+        table,
+        thead,
+        tr,
+        thTitle,
+        thAuthor,
+        thPages,
+        thBtn,
+        thBtnRead,
+    };
 }
 
 function tableRow() {
-  const tr = document.createElement('tr');
-  const tdTitle = document.createElement('td');
-  const tdAuthor = document.createElement('td');
-  const tdPages = document.createElement('td');
-  const tdBtn = document.createElement('td');
-  const tdBtnRead = document.createElement('td');
+    const tr = document.createElement('tr');
+    const tdTitle = document.createElement('td');
+    const tdAuthor = document.createElement('td');
+    const tdPages = document.createElement('td');
+    const tdBtn = document.createElement('td');
+    const tdBtnRead = document.createElement('td');
 
-  const removeBtn = document.createElement('button');
-  const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
 
-  return {
-    tr,
-    tdTitle,
-    tdAuthor,
-    tdPages,
-    tdBtn,
-    tdBtnRead,
-    removeBtn,
-    readBtn,
-  };
+    return {
+        tr,
+        tdTitle,
+        tdAuthor,
+        tdPages,
+        tdBtn,
+        tdBtnRead,
+        removeBtn,
+        readBtn,
+    };
 }
 
 function assignValues(i) {
-  const table = tableRow();
+    const table = tableRow();
 
-  table.tdTitle.textContent = myLibrary[i].title;
-  table.tdAuthor.textContent = myLibrary[i].author;
-  table.tdPages.textContent = myLibrary[i].pages;
-  table.tdBtn.appendChild(table.removeBtn);
-  table.tdBtnRead.appendChild(table.readBtn);
-  table.removeBtn.classList.add(...btnClass);
-  table.readBtn.classList.add(...readBtnClass);
-  table.removeBtn.textContent = 'Remove';
-  table.removeBtn.setAttribute('data-id', `${i}`);
-  if (myLibrary[i].isRead) {
-    table.readBtn.textContent = 'Read';
-  } else {
-    table.readBtn.textContent = 'Not Read';
-  }
-  table.readBtn.setAttribute('data-id', `${i}`);
-  return table;
+    table.tdTitle.textContent = myLibrary[i].title;
+    table.tdAuthor.textContent = myLibrary[i].author;
+    table.tdPages.textContent = myLibrary[i].pages;
+    table.tdBtn.appendChild(table.removeBtn);
+    table.tdBtnRead.appendChild(table.readBtn);
+    table.removeBtn.classList.add(...btnClass);
+    table.readBtn.classList.add(...readBtnClass);
+    table.removeBtn.textContent = 'Remove';
+    table.removeBtn.setAttribute('data-id', `${i}`);
+    if (myLibrary[i].isRead) {
+        table.readBtn.textContent = 'Read';
+    } else {
+        table.readBtn.textContent = 'Not Read';
+    }
+    table.readBtn.setAttribute('data-id', `${i}`);
+    return table;
 }
 
 function displayBooks() {
-  const tHead = tableHead();
-  const tbody = document.createElement('tbody');
+    const tHead = tableHead();
+    const tbody = document.createElement('tbody');
 
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    const table = assignValues(i);
-    table.readBtn.setAttribute('data-id', `${i}`);
-    table.readBtn.addEventListener('click', toggle);
-    table.removeBtn.addEventListener('click', (e) => {
-      myLibrary.splice(e.target.dataset.id, 1);
-      const table = document.querySelector('.table');
-      table.remove();
-      displayBooks();
-    });
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        const table = assignValues(i);
+        table.readBtn.setAttribute('data-id', `${i}`);
+        table.readBtn.addEventListener('click', toggle);
+        table.removeBtn.addEventListener('click', (e) => {
+            myLibrary.splice(e.target.dataset.id, 1);
+            const table = document.querySelector('.table');
+            table.remove();
+            displayBooks();
+        });
 
-    table.tr.append(table.tdTitle, table.tdAuthor, table.tdPages, table.tdBtn, table.tdBtnRead);
-    tbody.appendChild(table.tr);
-  }
+        table.tr.append(table.tdTitle, table.tdAuthor, table.tdPages, table.tdBtn, table.tdBtnRead);
+        tbody.appendChild(table.tr);
+    }
 
-  tHead.container.appendChild(tHead.table);
-  tHead.table.appendChild(tHead.thead);
-  tHead.thead.appendChild(tHead.tr);
-  tHead.tr.append(tHead.thTitle, tHead.thAuthor, tHead.thPages, tHead.thBtn, tHead.thBtnRead);
-  tHead.table.appendChild(tbody);
+    tHead.container.appendChild(tHead.table);
+    tHead.table.appendChild(tHead.thead);
+    tHead.thead.appendChild(tHead.tr);
+    tHead.tr.append(tHead.thTitle, tHead.thAuthor, tHead.thPages, tHead.thBtn, tHead.thBtnRead);
+    tHead.table.appendChild(tbody);
 }
 
 saveBtn.addEventListener('click', () => {
-  addBookToLibrary();
-  myLibrary.push(newBook);
-  $('#exampleModal').modal('hide');
-  const table = document.querySelector('.table');
-  table.remove();
+    addBookToLibrary();
+    myLibrary.push(newBook);
+    $('#exampleModal').modal('hide');
+    const table = document.querySelector('.table');
+    table.remove();
 
-  $('#exampleModal').on('hidden.bs.modal', function () {
-    $(this).find('form').trigger('reset');
-  });
+    $('#exampleModal').on('hidden.bs.modal', function() {
+        $(this).find('form').trigger('reset');
+    });
 
-  displayBooks();
+    displayBooks();
 });
 
 displayBooks();
